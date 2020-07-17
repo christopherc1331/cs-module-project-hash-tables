36fd391e6a9aa5355c44c66655f0f1a12fd3b323
Chris Cruz <christopherc1331@gmail.com>
2020-07-16 21:09:55 -0700

    finished no dups algo

--- applications/no_dups/no_dups.py
+++ applications/no_dups/no_dups.py
@@ -1,6 +1,21 @@
 def no_dups(s):
     # Your code here
+    s = s.split()
 
+    new_dict = {}
+
+    for i in s:
+        if i not in new_dict:
+            new_dict[i] = 0
+
+    new_string = ""
+
+    dict_items = new_dict.items()
+
+    for item in dict_items:
+        new_string += f"{item[0]} "
+
+    return new_string.strip()
 
 
 if __name__ == "__main__":
